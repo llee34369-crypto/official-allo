b282118158cd5aa7a6b42b5f74ad199058cf3bd8
llee34369-crypto <llee34369@gmail.com>
2026-04-26 01:25:39 +0800

    Add files via upload

--- app/allocation/page.tsx
+++ app/allocation/page.tsx
@@ -40,7 +40,7 @@ const ALLOCATION_TASK_STORAGE_KEY = 'speakerai-allocation-task-unlock';
 const X_HANDLE = 'SpeakerProtocol';
 const X_FOLLOW_URL = `https://x.com/intent/follow?screen_name=${X_HANDLE}`;
 const X_REPOST_URL =
-  'https://x.com/SpeakerAI_BNB/status/2041803442121748936?s=20';
+  'https://x.com/SpeakerProtocol/status/2048090313433006479?s=20';
 
 const SOCIAL_LINKS = {
   website: 'https://www.speakerai.org',
